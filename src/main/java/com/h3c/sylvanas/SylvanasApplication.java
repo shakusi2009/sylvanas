66506d6dd21ca49a25bfa9511fe287ca4ca59b6e
z10613 <1qaz2wsx>
2018-12-27 22:47:50 +0800

    [add]init...

---
+++ src/main/java/com/h3c/sylvanas/SylvanasApplication.java
@@ -0,0 +1,14 @@
+package com.h3c.sylvanas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SylvanasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SylvanasApplication.class, args);
+    }
+
+}
+
